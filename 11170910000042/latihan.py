915afa7f4d0eb88746927501facc848f1ee5ad18
Aulia Rahman <53137982+rahmanaul@users.noreply.github.com>
2020-03-17 17:35:37 +0700

    Add files via upload

---
+++ 11170910000042/latihan.py
@@ -0,0 +1,10 @@
+print("selamat datang di aplikasi penjumlahan dua bilangan")
+c = 1
+while c == 1:
+    a = input("Masukan Bilangan Pertama :")
+    b = input("Masukan Bilangan kedua :")
+    hasil = float(a) + float(b)
+    print("hasil dari ", a, " + ", b, " = ", hasil)
+    c = input(print("masukan angka 1 untuk kembali melakukan penjumlahan :"))
+else:
+    print("terimakasih")

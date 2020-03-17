5beef7a025f18f016999cc2173e9bf47b8de3786
rahmanaul <gumgumboy20@gmail.com>
2020-03-17 15:57:16 +0700

    First Commit

---
+++ latihan.py
@@ -0,0 +1,10 @@
+c = 1
+while c is 1:
+    print("aplikas penjumlahan dua angka")
+    a = int(input("masukan angka pertama: "))
+    b = int(input("masukan angka kedua: "))
+    sum = a + b
+    print(a, "+", b, "hasilnya", sum)
+    c = int(input("masukan angka 1 jika ingin menghtung kembali :"))
+else:
+    print("Terimakasih")

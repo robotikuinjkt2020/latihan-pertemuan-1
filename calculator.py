baa768b831b70030b8ec24a70a2c32b5b46b1125
fauzana18 <fauzana18@gmail.com>
2020-03-17 20:02:19 +0700

    no message

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#Program calculator oleh Ahmad Fauzan Maulana (11170910000038)
+
+x=int(input("Masukkan x: "))
+y=int(input("Masukkan y: "))
+
+print("x + y = " + str(x+y))
+print("x - y = " + str(x-y))
+print("x X y = " + str(x*y))
+print("x : y = " + str(x//y) + " sisa " + str(x%y))

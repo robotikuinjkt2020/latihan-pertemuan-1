3a3b8cb8796fa9b38986ba8f069315ca2003da9f
Aulia Rahman <53137982+rahmanaul@users.noreply.github.com>
2020-03-17 17:42:05 +0700

    Update latihan.py

--- 11170910000042/latihan.py
+++ 11170910000042/latihan.py
@@ -1,10 +1,22 @@
-print("selamat datang di aplikasi penjumlahan dua bilangan")
 c = 1
 while c == 1:
-    a = input("Masukan Bilangan Pertama :")
-    b = input("Masukan Bilangan kedua :")
-    hasil = float(a) + float(b)
-    print("hasil dari ", a, " + ", b, " = ", hasil)
-    c = input(print("masukan angka 1 untuk kembali melakukan penjumlahan :"))
+    print("aplikas penjumlahan dua angka")
+    try:
+        a = float(input("masukan angka pertama: "))
+    except ValueError:
+        print("input salah, masukan angka pertama : ")
+        a = float(input(
+            "masukan angka pertama jika anda kembali salah menginput maka anda akan keluar: "))
+    else:
+        try:
+            b = float(input("masukan angka kedua: "))
+        except ValueError:
+            print("input salah, masukan angka pertama : ")
+            b = float(input(
+                "masukan angka pertama jika anda kembali salah menginput maka anda akan keluar "))
+        else:
+            sum = a + b
+            print(a, "+", b, "hasilnya", sum)
+            c = float(input("masukan angka 1 jika ingin menghtung kembali :"))
 else:
-    print("terimakasih")
+    print("Terimakasih")

a0065a762ab1ad7f19c9e2aa131ae798a99946eb
djrl10 <60725772+djrl10@users.noreply.github.com>
2020-03-20 18:16:51 +0700

    Add files via upload

---
+++ 11170910000027/penjumlahan.py
@@ -0,0 +1,7 @@
+ 
+bil1 = input('Masukkan bilangan pertama: ') 
+bil2 = input('Masukkan bilangan kedua: ') 
+# Menjumlahkan bilangan 
+jumlah = float(bil1) + float(bil2) 
+# Menampilkan jumlah 
+print('Jumlah {0} + {1} adalah {2}'.format(bil1, bil2, jumlah))

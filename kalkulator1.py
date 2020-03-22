d4fbf70e762a5a2888ea3bd70e2ff54449ea52ab
juandela17 <47452930+juandela17@users.noreply.github.com>
2020-03-22 13:26:05 +0700

    no message

---
+++ kalkulator1.py
@@ -0,0 +1,36 @@
+def penjumlahan(a, b):
+    penjumlahan = float(a) + float(b)
+    return penjumlahan
+
+def pengurangan(a, b):
+    pengurangan =float (a) -float (b)
+    return pengurangan
+
+
+def perkalian(a, b):
+    perkalian = float(a) * float(b)
+    return perkalian
+
+
+def pembagian(a, b):
+    pembagian = float(a) / float(b)
+    return pembagian
+
+
+lagi = 'ya'
+while lagi == 'ya':
+    print(' \t \t Program Kalkulator Yuhuuu')
+    a = input('Masukan Bilangan 1: ')
+    b = input('Masukan Bilangan 2 : ')
+    print(' 1. penjumlahan \n 2. pengurangan \n 3. perkalian \n 4. pembagian \n')
+    c = input('Pilih 1-4 : ')
+    if c == '1':
+        print(' Hasilnya adalah = ', penjumlahan(a, b))
+    elif c == '2':
+        print(' Hasilnya adalah = ', pengurangan(a, b))
+    elif c == '3':
+        print(' Hasilnya adalah = ', perkalian(a, b))
+    else :
+        print(' Hasilnya adalah = ', pembagian(a, b))
+
+    lagi = raw_input(" Mau Lagi ? [ya/tidak] : ")
